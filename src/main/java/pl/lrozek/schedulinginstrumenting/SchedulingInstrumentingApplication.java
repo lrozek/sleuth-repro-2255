0f3895e065e3e1008b7d7cb42ae38111fffecddf
Łukasz Rożek <lukasz.rozek@consileon.pl>
2023-01-25 12:06:11 +0100

    init

---
+++ src/main/java/pl/lrozek/schedulinginstrumenting/SchedulingInstrumentingApplication.java
@@ -0,0 +1,13 @@
+package pl.lrozek.schedulinginstrumenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingInstrumentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingInstrumentingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lrozek/schedulinginstrumenting/SchedulingInstrumentingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lrozek.schedulinginstrumenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingInstrumentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
